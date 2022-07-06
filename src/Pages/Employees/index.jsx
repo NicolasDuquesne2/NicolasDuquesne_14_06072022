4513749a6a4d7207213b20530d65df69b4eea01a
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2022-07-06 17:43:46 +0200

    Set The custom header for emplyees page

--- src/Pages/Employees/index.jsx
+++ src/Pages/Employees/index.jsx
@@ -4,8 +4,7 @@ import Header from "../../Components/Header"
 function Employees() {
     return (
         <React.Fragment>
-            <h1>Current Employees</h1>
-            <a href="/">Home</a>
+            <Header params={{title: "Current Employees", link: {text: "Home", href: "/"}}} />
             <div className="container">
 
             </div>
