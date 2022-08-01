6d10463d2c2ee1cac8fafcaf6b4b812eb0437b02
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2022-08-01 17:12:02 +0200

    The useGetObjectKeys hook is deleted. No hook folder in the main project

--- src/Hooks/useGetObjectKeys.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react"
-
-export const useGetObjectKeys = (object) => {
-    const [keys, setKeys] = useState([])
-
-    setKeys(Object.keys(object))
-
-    return [keys]
-} 
