b3f9b6bdb442304dc88a509349bb7521f3000feb
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2022-08-01 13:13:21 +0200

    TableExtension become an independent component from the app

--- src/Components/TableExtension/TableHeader/index.jsx
+++ src/Components/TableExtension/TableHeader/index.jsx
@@ -1,22 +1,19 @@
-import { useSelector, useDispatch } from "react-redux/es/exports"
-import { setOrder } from "../../Redux/Table/orderSlice"
-import { setField } from "../../Redux/Table/fieldSlice"
+import { useState } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faSort, faSortDown, faSortUp } from '@fortawesome/free-solid-svg-icons'
 import "./tableheader.css"
 
 function TableHeader({columns, handleTableEvent}) {
 
-    const sortField = useSelector(state => state.field.value)
-    const order = useSelector(state => state.order.value)
-    const dispatch = useDispatch()
+    const [sortField, setSortField] = useState("")
+    const [order, setOrder] = useState("asc")
 
     let icon = null
 
     const handleSortingChange = (accessor) => {
         const switchOrder = accessor === sortField && order === "asc" ? "desc": "asc"
-        dispatch(setField(accessor))
-        dispatch(setOrder(switchOrder))
+        setSortField(accessor)
+        setOrder(switchOrder)
         handleTableEvent({name: "sort", sortField: accessor, sortOrder: switchOrder})
     }
 
